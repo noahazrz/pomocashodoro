a466acef68164dda9f3ae2983c32e9cf9cb490b1
Noah <87227367+noahazrz@users.noreply.github.com>
2026-08-25 19:44:31 +0200

    Refactor Firebase config for Compat mode
    Updated Firebase initialization to use Compat mode and exposed auth and db globally.

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAAYJMpeVZ9z5XY6nm8m6kA1kVmCn-3x3I",
   authDomain: "pomocashodoro.firebaseapp.com",
@@ -16,6 +9,9 @@ const firebaseConfig = {
   measurementId: "G-J638W7V6KT"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase (Compat mode)
+firebase.initializeApp(firebaseConfig);
+
+// Expose auth and db globally for app.js
+const auth = firebase.auth();
+const db = firebase.firestore();
